9680a60d9607452bf69f4446078782f0e52211fe
Nicolas Lopes <nicolaslopesbusiness@gmail.com>
2022-06-05 12:36:41 -0300

    feat: building schema from generated resolvers

--- src/index.ts
+++ src/index.ts
@@ -1,22 +1,10 @@
-import { gql, ApolloServer } from 'apollo-server';
+import 'reflect-metadata';
+import { ApolloServer } from 'apollo-server';
+import { buildGraphQLSchema } from './graphql/buildSchema';
 
-const typeDefs = gql`
-  type Book {
-    title: String
-    author: String
-  }
-  type Query {
-    books: [Book]
-  }
-`;
+(async () => {
+  const schema = await buildGraphQLSchema();
+  const server = new ApolloServer({ schema });
 
-const server = new ApolloServer({
-  typeDefs,
-  resolvers: {
-    Query: {
-      books: () => [{ title: 'a', author: 'b' }],
-    },
-  },
-});
-
-server.listen().then(({ url }) => console.log(`🚀  Server ready at ${url}`));
+  server.listen().then(({ url }) => console.log(`🚀  Server ready at ${url}`));
+})();
