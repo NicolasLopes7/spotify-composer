c6c55e675fac24345bb6b723a1db4a53d479268c
Nicolas Lopes Aquino <57234795+NicolasLopes7@users.noreply.github.com>
2022-06-05 12:58:02 -0300

    feat/wire up schema first with prisma (#2)
    * feat: building schema from generated resolvers
    
    * feat: wire up schema-first with typegraphql and prisma

--- src/index.ts
+++ src/index.ts
@@ -1,22 +1,13 @@
-import { gql, ApolloServer } from 'apollo-server';
+import 'reflect-metadata';
+import { ApolloServer } from 'apollo-server';
+import { buildGraphQLSchema } from './graphql/buildSchema';
+import { prisma } from './config/prisma';
+(async () => {
+  const schema = await buildGraphQLSchema();
+  const server = new ApolloServer({
+    schema,
+    context: { prisma },
+  });
 
-const typeDefs = gql`
-  type Book {
-    title: String
-    author: String
-  }
-  type Query {
-    books: [Book]
-  }
-`;
-
-const server = new ApolloServer({
-  typeDefs,
-  resolvers: {
-    Query: {
-      books: () => [{ title: 'a', author: 'b' }],
-    },
-  },
-});
-
-server.listen().then(({ url }) => console.log(`🚀  Server ready at ${url}`));
+  server.listen().then(({ url }) => console.log(`🚀  Server ready at ${url}`));
+})();
