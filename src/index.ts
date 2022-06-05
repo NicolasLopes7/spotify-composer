88170ac1e28866a268fe8ef474a887d58ac27b9e
Nicolas Lopes <nicolaslopesbusiness@gmail.com>
2022-06-05 12:56:35 -0300

    feat: wire up schema-first with typegraphql and prisma

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,13 @@
 import 'reflect-metadata';
 import { ApolloServer } from 'apollo-server';
 import { buildGraphQLSchema } from './graphql/buildSchema';
-
+import { prisma } from './config/prisma';
 (async () => {
   const schema = await buildGraphQLSchema();
-  const server = new ApolloServer({ schema });
+  const server = new ApolloServer({
+    schema,
+    context: { prisma },
+  });
 
   server.listen().then(({ url }) => console.log(`🚀  Server ready at ${url}`));
 })();
